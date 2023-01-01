ff6c07f2b15001621d22d3f89e69d24f6b3a1da8
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-01-01 18:47:21 +0530

    chore: :wrench: add tailwind colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,25 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "fira-code": "'Fira Code', monospace",
+        "manrope": "'Manrope', sans-serif"
+      },
+      colors: {
+        primaryDark: "#1C1B2B",
+        secondaryDark: "#10111A",
+        primaryLight: "#F4F6FA",
+        secondaryLight: "#F1F2F6",
+        primaryOrange: "#FC5830",
+        lightGrey: "#B5BAC7",
+        white: "#FFFFFF",
+        primaryYellow: "#F6BA3D",
+        semiTransparent: "rgba(0, 0, 0, 0.6)",
+        primaryGreen: "#08CD92",
+        primaryRed: "#D61C4E"
+      }
+    },
   },
   plugins: [],
 }
